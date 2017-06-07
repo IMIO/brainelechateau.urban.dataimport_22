5c572defbc80adb2e05d8a4c7a747f6b0387cc4c
jjaumotte <julien.jaumotte@imio.be>
2017-06-07 14:05:01 +0200

    Add csv importer profile for locality's 2.2 version

---
+++ src/brainelechateau/urban/dataimport/csv/settings.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from imio.urban.dataimport.csv.importer import CSVDataImporter
+from imio.urban.dataimport.csv.interfaces import ICSVImporter
+from imio.urban.dataimport.browser.adapter import ImporterFromSettingsForm
+from imio.urban.dataimport.browser.import_panel import ImporterSettings
+
+from zope.interface import implements
+
+from imio.urban.dataimport.csv.settings import CSVImporterFromImportSettings
+
+
+class CSVImporterSettings(ImporterSettings):
+    """
+    """
+    file_type = None
+
+class BrainelechateauCsvImporterFromImportSettings(CSVImporterFromImportSettings):
+    """ """
+
+    def get_importer_settings(self):
+        """
+        Return the db name to read.
+        """
+        CSVImporterSettings.file_type = 'new'
+        settings = super(BrainelechateauCsvImporterFromImportSettings, self).get_importer_settings()
+        db_settings = {
+            'key_column': 'id',
+            'csv_filename': 'BLC-TOUS',
+            # 'csv_filename': 'brainelechateau_new_20170223.csv',
+        }
+
+        settings.update(db_settings)
+
+        return settings
+
+
+class BrainelechateauOldCsvImporterFromImportSettings(CSVImporterFromImportSettings):
+    """ """
+
+    def get_importer_settings(self):
+        """
+        Return the db name to read.
+        """
+        CSVImporterSettings.file_type = 'old'
+        settings = super(BrainelechateauOldCsvImporterFromImportSettings, self).get_importer_settings()
+        db_settings = {
+            'key_column': 'id',
+            'csv_filename': 'brainelechateau_old_20170223.csv',
+        }
+
+        settings.update(db_settings)
+
+        return settings
